e19c18a853984ac80fb973c9e350ebd6f221c28d
Briws <brightwiths@gmail.com>
2021-12-07 18:29:22 +0300

    Upgrade to react-router-dom v6.

--- src/n1-main/m1-ui/p2-main/Main.tsx
+++ src/n1-main/m1-ui/p2-main/Main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Header} from "../p1-header/Header";
-import {Redirect, Route, Switch} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {Login} from "../../../n2-features/f1-auth/a1-login/Login";
 import {Registration} from "../../../n2-features/f1-auth/a2-register/Registration";
 import {Profile} from "../../../n2-features/f1-auth/Profile";
@@ -13,17 +13,17 @@ export const Main = () => {
     return (
         <div>
             <Header/>
-            <Switch>
-                <Route exact path={'/login'} render={() => <Login/>}/>
-                <Route exact path={'/registration'} render={() => <Registration/>}/>
-                <Route exact path={'/profile'} render={() => <Profile/>}/>
-                <Route exact path={'/404'} render={() => <Page404/>}/>
-                <Route exact path={'/passwordrecovery'} render={() => <PasswordRecovery/>}/>
-                <Route exact path={'/passwordnew'} render={() => <PasswordNew/>}/>
-                <Route exact path={'/tests'} render={() => <Examples/>}/>
-                <Redirect exact from={'/'} to={'/tests'}/>
-                <Redirect from={'*'} to={'/404'}/>
-            </Switch>
+            <Routes>
+                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/registration'} element={<Registration/>}/>
+                <Route path={'/profile'} element={<Profile/>}/>
+                <Route path={'/404'} element={<Page404/>}/>
+                <Route path={'/passwordrecovery'} element={<PasswordRecovery/>}/>
+                <Route path={'/passwordnew'} element={<PasswordNew/>}/>
+                <Route path={'/tests'} element={<Examples/>}/>
+                <Route path="/" element={<Navigate replace to="/tests" />} />
+                <Route path="*" element={<Navigate replace to="/404" />} />
+            </Routes>
         </div>
     );
 }
