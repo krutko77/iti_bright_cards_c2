30f323b8f7b508394d1b9fa82fd286d76c410d98
ivanin_j <ivanin.jay@gmail.com>
2021-12-11 14:40:56 +0200

    first commit

---
+++ src/n1-main/m2-bll/backUpPassWordReducer.ts
@@ -0,0 +1,18 @@
+const initialState: InitialStateType = {
+    testValue: ''
+}
+
+export const backUpPassWordReducer = (state = initialState, action: ActionType) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export const setTestValueAC = (newValue: string) =>
+    ({type: 'SET-TEST-VALUE', newValue} as const)
+
+
+type InitialStateType = {}
+
+type ActionType = ReturnType<typeof setTestValueAC>
