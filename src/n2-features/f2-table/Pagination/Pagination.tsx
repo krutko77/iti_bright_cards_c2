0970724ff1aa6c4fe301301b073a995bb85fd8a5
Briws <brightwiths@gmail.com>
2021-12-20 22:51:26 +0300

    New branch for Find and Pagination

---
+++ src/n2-features/f2-table/Pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+
+const Pagination = () => {
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+
+        </div>
+    )
+}
