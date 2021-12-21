dc883a30b6e2769620e5d06db88e1ba9a8aacbdd
Briws <brightwiths@gmail.com>
2021-12-21 21:53:19 +0300

    pagination progress

--- src/n2-features/f2-table/Pagination/Pagination.tsx
+++ src/n2-features/f2-table/Pagination/Pagination.tsx
@@ -3,53 +3,51 @@ import {useDispatch, useSelector} from "react-redux";
 import {AppStoreType} from "../../../n1-main/m2-bll/store";
 import {setCurrentPageAC} from "../../../n1-main/m2-bll/findAndPaginationReducer";
 import s from './Paginations.module.scss'
+import SuperButton from "../../../n1-main/m1-ui/common/c2-SuperButton/SuperButton";
 
 export const Pagination = () => {
     const dispatch = useDispatch()
-
-
-    const portionSize = 5; // порция которая видна в пагинации
-
-    // кол-во элементов на одной стр
+    
+    // count of elements at one page 
     let pageCount = useSelector<AppStoreType, number>(state => state.findAndPagination.pageCount)
-    // кол-во колод
+    // count of Card Packs
     let cardPacksTotalCount = useSelector<AppStoreType, number>(state => state.findAndPagination.cardPacksTotalCount)
-    // выбранная страница
+    // selected page
     let page = useSelector<AppStoreType, number>(state => state.findAndPagination.page)
-
+    
     const currentPageHandler = (page: number) => {
         dispatch(setCurrentPageAC(page))
     }
 
-    let portionCount = Math.ceil(cardPacksTotalCount / pageCount); //количество страниц всех!!! до пагинатора
+    let pagesCount = Math.ceil(cardPacksTotalCount / pageCount); // count of ALL pages, before the paginator
     let pages = [];
-    for (let i = 1; i <= portionCount; i++) {
+    for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
+    const portionSize = 5; // portion that is seeing in pagination
+    const portionCount = Math.ceil(pagesCount / portionSize) // count of portions per portionSize pages
 
-    const [portionNumber, setPortionNumber] = useState(1)
-
-    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
-    let rightPortionPageNumber = portionNumber * portionSize
-
+    const [portion, setPortion] = useState(1)
+    const leftNumber = (portion - 1) * portionSize + 1
+    const rightNumber = portion * portionSize
 
     return (
         <div className={s.pagination}>
-            {portionNumber > 1 &&
-              <button onClick={() => {
-                  setPortionNumber(portionNumber - 1)
-              }}>PREV </button>}
+            {portion > 1 &&
+              <SuperButton className={s.btn} onClick={() => {
+                  setPortion(portion - 1)
+              }}>&lt;</SuperButton>}
 
             {pages
-                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                .filter(p => p ? p >= leftNumber && p <= rightNumber : '')
                 .map(p => {
                     return <div key={p} className={`${s.item} ${page === p ? s.select : s.item}`}
                                 onClick={() => {currentPageHandler(p)}}>{p}</div>
                 })}
-            {portionCount > portionNumber &&
-              <button onClick={() => {
-                  setPortionNumber(portionNumber + 1)
-              }}>NEXT</button>}
+            {portionCount > portion &&
+              <SuperButton className={s.btn}  onClick={() => {
+                  setPortion(portion + 1)
+              }}>&gt;</SuperButton>}
         </div>
     )
 }
