6458f7e543211f35142c7ac2ea60915a8c2030e4
Pavel <krutko7779@gmail.com>
2021-12-25 15:20:11 +0300

    changes 25/12/2021

--- src/assets/components/packs-list/pagination/UsePagination.jsx
+++ src/assets/components/packs-list/pagination/UsePagination.jsx
@@ -5,7 +5,6 @@ import { styled } from '@mui/material/styles';
 
 const List = styled('ul')({
    listStyle: 'none',
-   // paddingLeft: "20px",
    margin: 0,
    display: 'flex',
    alignItems: "center",
@@ -18,62 +17,46 @@ export default function UsePagination() {
    });
 
    return (
-      <div className={s.UsePagination}>
-         <nav >
-            <List >
-               {items.map(({ page, type, selected, ...item }, index) => {
-                  let children = null;
-
-                  if (type === 'start-ellipsis' || type === 'end-ellipsis') {
-                     children = '…';
-                  } else if (type === 'page') {
-                     children = (
-                        <button
-                           type="button"
-                           style={{
-                              borderWidth: 0,
-                              backgroundColor: selected ? "#21268F" : "transparent",
-                              color: selected ? "#F2F5F7" : "#2D2E46",
-                              width: "24px",
-                              height: "24px",
-                              borderRadius: "3px",
-                              fontFamily: "Lato",
-                              fontSize: "12px",
-                              lineHeight: "120%",
-                              cursor: "pointer",
-                           }}
-                           {...item}
-                        >
-                           {page}
-                        </button>
-                     );
-                  }
-                  //  else {
-                  //    children = (
-                  //      <button type="button" {...item}>
-                  //         style={{
-                  //             background: "red"
-                  //                }}
-                  //        {type}
-                  //      </button>
-                  //    );
-                  //  }  
-                  else {
-            children = (
-              <button type="button" {...item}>
-                {type}
-              </button>
-            );
-          }                
-
-                  return <li
-                     style={{
-                        marginRight: "15px",
-                     }}
-                     key={index}>{children}</li>;
-               })}
-            </List>
-         </nav>
-      </div>
+      <nav className={s.UsePagination}>
+         <List >
+            {items.map(({ page, type, selected, ...item }, index) => {
+               let children = null;
+               if (type === 'start-ellipsis' || type === 'end-ellipsis') {
+                  children = '…';
+               } else if (type === 'page') {
+                  children = (
+                     <button
+                        type="button"
+                        style={{
+                           borderWidth: 0,
+                           backgroundColor: selected ? "#21268F" : "transparent",
+                           color: selected ? "#F2F5F7" : "#2D2E46",
+                           width: "24px",
+                           height: "24px",
+                           borderRadius: "3px",
+                           fontFamily: "Lato",
+                           fontSize: "12px",
+                           lineHeight: "120%",
+                           cursor: "pointer",
+                        }}
+                        {...item}
+                     >
+                        {page}
+                     </button>
+                  );
+               } else {
+                  children = (
+                     <button type="button" {...item}>
+                        {type}
+                     </button>
+                  );
+               } return <li
+                  style={{
+                     marginRight: "15px",
+                  }}
+                  key={index}>{children}</li>;
+            })}
+         </List>
+      </nav>
    );
 }
