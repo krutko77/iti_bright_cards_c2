8b3931fd86fa36570c2c487e157dcb944c19e004
Pavel <krutko7779@gmail.com>
2022-01-04 00:02:33 +0300

    Header corrected

--- src/n1-main/m1-ui/p1-header/Header.tsx
+++ src/n1-main/m1-ui/p1-header/Header.tsx
@@ -1,39 +1,56 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import s from './Header.module.scss'
-import {useDispatch, useSelector} from "react-redux";
-import {LogoutTC} from "../../m2-bll/authReducer";
-import {AppStoreType} from "../../m2-bll/store";
-import {LinearProgress} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { LogoutTC } from "../../m2-bll/authReducer";
+import { AppStoreType } from "../../m2-bll/store";
+import { LinearProgress } from "@mui/material";
 
 export const Header = () => {
-    const dispatch = useDispatch()
-    const isLoggedIn = useSelector<AppStoreType, boolean>(state => state.auth.isLoggedIn)
+   const dispatch = useDispatch()
+   const isLoggedIn = useSelector<AppStoreType, boolean>(state => state.auth.isLoggedIn)
 
-    const onLogout = () => {
-        dispatch(LogoutTC())
-    }
+   const onLogout = () => {
+      dispatch(LogoutTC())
+   }
 
-    return (
-        <div className={s.header}>
-            <div className={s.logo}>BrightCards</div>
-            <div className={s.links}>
-                {isLoggedIn
-                    ? <>
+   return (
+      <div className={s.header}>
+         <div className={s.logo}>BrightCards</div>
+         <nav className={s.navigation}>
+            <ul className={s.list}>
+               {isLoggedIn
+                  ? <>
+                     <li>
                         <NavLink className={s.item} to={'/profile'}>Profile</NavLink>
+                     </li>
+                     <li>
                         <a className={s.link} onClick={onLogout}>Log out</a>
-                    </>
-                    : <>
+                     </li>
+                  </>
+                  : <>
+                     <li>
                         <NavLink className={s.item} to={'/login'}>Login</NavLink>
+                     </li>
+                     <li>
                         <NavLink className={s.item} to={'/registration'}>Registration</NavLink>
+                     </li>
+                     <li>
                         <NavLink className={s.item} to={'/passwordrecovery'}>Recovery Password</NavLink>
-                    </>
-                }
-                <NavLink className={s.item} to={'/packs'}>Packs</NavLink>
-                <NavLink className={`${s.item} ${s.notDone}`} to={'/packsdesigned'}>PacksDesigned</NavLink>
-                {/*<NavLink className={s.item} to={'/cards'}>Cards</NavLink>*/}
-                {/*<NavLink className={s.item} to={'/tests'}>Tests</NavLink>*/} {/*type /test to see*/}
-            </div>
-        </div>
-    );
+                     </li>
+                  </>
+               }
+
+               <li>
+                  <NavLink className={s.item} to={'/packs'}>Packs</NavLink>
+               </li>
+               <li>
+                  <NavLink className={`${s.item} ${s.notDone}`} to={'/packsdesigned'}>PacksDesigned</NavLink>
+               </li>
+               {/*<NavLink className={s.item} to={'/cards'}>Cards</NavLink>*/}
+               {/*<NavLink className={s.item} to={'/tests'}>Tests</NavLink>*/} {/*type /test to see*/}
+            </ul>
+         </nav>
+      </div>
+   );
 }
